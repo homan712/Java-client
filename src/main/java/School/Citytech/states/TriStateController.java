839ac1bfe0c8b5510c1939f3abfedb7cb7b985f5
homan712 <68459928+homan712@users.noreply.github.com>
2021-12-16 16:09:11 -0500

    fix the problem, to count.getAndIncrement, result = 1

--- src/main/java/School/Citytech/states/TriStateController.java
+++ src/main/java/School/Citytech/states/TriStateController.java
@@ -6,13 +6,7 @@ import School.Citytech.retirement.CustomTableCell;
 import School.Citytech.states.model.Property;
 
 import School.Citytech.states.model.TriState;
-import javafx.beans.Observable;
-import javafx.beans.binding.Bindings;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -72,121 +66,101 @@ public class TriStateController extends MainController implements Initializable 
 
 
     void refreshData(Predicate<Property>predicate){
-                var list = Arrays.stream(gList).filter(nyTriState).toList();
+        var list = Arrays.stream(gList).filter(nyTriState).toList();
 
-                var isPopulated = pieTriState.getData().size() > 0;
+        var isPopulated = pieTriState.getData().size() > 0;
 
-                if(!isPopulated)
-               for (var property: list){
-                       PieChart.Data slice = new PieChart.Data(property.getState(),
-                               property.getSumOfNetIncome());
-                       pieTriState.getData().add(slice);
-               }
-
-
-               tvStateInfo.getItems().clear();
-
-               var property = TriStateDataLayer.getProperty();
-               var listOfProperties = Arrays.stream(property).filter(predicate).toList();
-               ObservableList<Property> oList = tvStateInfo.getItems();
-               oList.addAll(listOfProperties);
-
-                tcCost.setCellFactory( column-> {
-                    var cell = new CustomTableCell();
-                    return cell;
-                });
-
-                tcNetIncome.setCellFactory( column-> {
-                    var cell = new CustomTableCell();
-                    return cell;
-                });
-
-                tcPercentage.setCellFactory( column-> {
-                    var cell = new PercentageTableCell();
-                    return cell;
-                });
-
-
-        }
-        AtomicInteger count;
-
-        @Override
-        public void initialize(URL url, ResourceBundle resourceBundle) {
-             count = new AtomicInteger();
-                System.out.println("confirmed");
-                this.refreshData( e -> true);
-                this.pieTriState.getData().forEach(this::clickOnPie);
-
-                Arrays.stream(gList).forEach(e -> {
-                    var checkBox = new CheckBox(e.getState());
-                    checkBox.setPrefSize(60,15);
-                    this.fpStates.getChildren().add(checkBox);
-                    System.out.println(checkBox);
-
-                    checkBox.setOnAction((ActionEvent event) -> {
-                        if(checkBox.isSelected()){
-                            count.getAndIncrement();
-
-                            System.out.println("State "+checkBox.getText());
-
-                            count.getAndIncrement();
-                            if (count.getAndIncrement() >= 4){
-
-                            }
-                            //btnRefresh.setVisible(false);
-                            //lblStatus.setText("more than 4 states selected is invalid.");
-
-                        }
-
-                    });
-
-                    int NUM_CHECKBOXES = 4;
-
-                    ObservableList<CheckBox> checkBoxes = FXCollections.observableArrayList(
-                            c -> new Observable[] { c.selectedProperty() }
-                    );
-                    FilteredList<CheckBox> selectedCheckBoxes = checkBoxes.filtered(
-                            CheckBox::isSelected
-                    );
-                    IntegerProperty numCheckboxes = new SimpleIntegerProperty();
-                    numCheckboxes.bind(Bindings.size(checkBoxes));
-                    IntegerProperty numSelectedCheckboxes = new SimpleIntegerProperty();
-                    numSelectedCheckboxes.bind(Bindings.size(selectedCheckBoxes));
-
-                    // create the checkboxes.
-
-                    for (int i = 0; i < NUM_CHECKBOXES; i++) {
-                        checkBoxes.add(new CheckBox("" + (i+1)));
-                    }
-
-                    System.out.println("ON "+numSelectedCheckboxes);
-
-                });
-
+        if(!isPopulated)
+            for (var property: list){
+                PieChart.Data slice = new PieChart.Data(property.getState(),
+                        property.getSumOfNetIncome());
+                pieTriState.getData().add(slice);
             }
 
 
-        void clickOnPie (PieChart.Data data){
-                EventHandler<MouseEvent> eventHandler = evt -> {
-                        System.out.println(evt);
-                        System.out.println("name " + data.getName());
-                        System.out.println("pieValue " + data.getPieValue());
-                        Predicate<Property> predicate = e -> e.getState().equals(data.getName());
-                        this.refreshData(predicate);
+        tvStateInfo.getItems().clear();
 
-                        // #6 lbltitle change name
-                        lblTitle.setText("TriState Income For: " + data.getName());
-                };
-            data.getNode().addEventHandler(MouseEvent.MOUSE_CLICKED,eventHandler);
+        var property = TriStateDataLayer.getProperty();
+        var listOfProperties = Arrays.stream(property).filter(predicate).toList();
+        ObservableList<Property> oList = tvStateInfo.getItems();
+        oList.addAll(listOfProperties);
 
-        }
+        tcCost.setCellFactory( column-> {
+            var cell = new CustomTableCell();
+            return cell;
+        });
+
+        tcNetIncome.setCellFactory( column-> {
+            var cell = new CustomTableCell();
+            return cell;
+        });
+
+        tcPercentage.setCellFactory( column-> {
+            var cell = new PercentageTableCell();
+            return cell;
+        });
+
+
+    }
+    AtomicInteger count ;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        count = new AtomicInteger();
+        System.out.println("confirmed");
+        this.refreshData( e -> true);
+        this.pieTriState.getData().forEach(this::clickOnPie);
+
+        Arrays.stream(gList).forEach(e -> {
+            var checkBox = new CheckBox(e.getState());
+            checkBox.setPrefSize(60,15);
+            this.fpStates.getChildren().add(checkBox);
+            System.out.println(checkBox);
+
+            checkBox.setOnAction((ActionEvent event) -> {
+                if(checkBox.isSelected()){
+
+
+
+                    System.out.println("State "+checkBox.getText());
+
+
+                    if (count.getAndIncrement() >= 4){
+
+
+
+                    }
+                    //btnRefresh.setVisible(false);
+                    //lblStatus.setText("more than 4 states selected is invalid.");
+
+                }
+
+            });
+
+
+        });
+
+    }
+
+    void clickOnPie (PieChart.Data data){
+        EventHandler<MouseEvent> eventHandler = evt -> {
+            System.out.println(evt);
+            System.out.println("name " + data.getName());
+            System.out.println("pieValue " + data.getPieValue());
+            Predicate<Property> predicate = e -> e.getState().equals(data.getName());
+            this.refreshData(predicate);
+
+            // #6 lbltitle change name
+            lblTitle.setText("TriState Income For: " + data.getName());
+        };
+        data.getNode().addEventHandler(MouseEvent.MOUSE_CLICKED,eventHandler);
+
+    }
 
     @FXML
     void onClick(ActionEvent event) {
-        System.out.println(count);
+        System.out.println(count.get());
     }
 
 }
-
-
-
